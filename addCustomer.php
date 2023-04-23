8d27dc8fbe4b347c8cb7223f55ae2efaf9952be4
DianoEdriel <92987713+DianoEdriel@users.noreply.github.com>
2023-04-23 21:00:09 +0800

    addcustomerbranch

---
+++ addCustomer.php
@@ -0,0 +1,29 @@
+<!Doctype html>
+<html lang="en">
+  <head>
+
+   <!-- Bootstrap CSS -->
+   <link rel="stylesheet"  type="text/css" href="./css/bootstrap.min.css">
+    <script type="text/javascript" src="./js/bootstrap.min.js"></script>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+   
+
+    <title>Hello, world!</title>
+  </head>
+  <body>
+    <?php
+        include 'include/navbar.html';
+    ?>
+    <div class="container-fluid ">
+        <div class="row">
+            <div class="col-sm-6 border border-primary">
+                Hello
+            </div>
+        </div>
+    </div>
+    
+  </body>
+</html>
