390a5ea8fd4bb83356540d86b23d9e0c0e5fc0db
DianoEdriel <92987713+DianoEdriel@users.noreply.github.com>
2023-05-01 00:32:24 +0800

    .

--- dashboard.php
+++ dashboard.php
@@ -14,17 +14,16 @@
 </head>
 
 <body>
-  <div class="container-fluid border">
-    <div class="container-fluid">
-      <div class="row">
-        <div class="col-md-3 border border-primary">
-          hello
-        </div>
-        <div class="col-md-9 border border-danger">
-          <?php
-            include 'addCustomer.html';
-          ?>
-        </div>
+  <div class="container-fluid " >
+    <div class="row" >
+      <div class="col-md-3 border border-danger" >1
+
+      </div>
+      <div class="col-md-9 border border-primary" >
+        <?php
+          include 'addCustomer.html';
+        ?>
+        
       </div>
     </div>
   </div>
