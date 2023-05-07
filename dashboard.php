e475d7188c281f7051256166b3693a10a6f1f668
DianoEdriel <92987713+DianoEdriel@users.noreply.github.com>
2023-05-07 23:25:52 +0800

    m

--- dashboard.php
+++ dashboard.php
@@ -10,14 +10,15 @@
       include './include/head.html';
       
     ?>
+    
   <title>DashBoard</title>
 </head>
 
 <body>
   <div class="container-fluid " >
     <div class="row" >
-      <div class="col-md-3 border border-danger" >1
-
+      <div class="col-md-3 border border-danger" >
+        <?php include 'sidebar.html'?>
       </div>
       <div class="col-md-9 border border-primary" >
         <?php
